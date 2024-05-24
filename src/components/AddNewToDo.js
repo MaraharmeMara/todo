5e56c585308fa70b6c9e892b019ddf9e0e8e0b77
MaraharmeMara <74717531+MaraharmeMara@users.noreply.github.com>
2024-05-24 12:20:22 +0300

    changed colors+added modal component

--- src/components/AddNewToDo.js
+++ src/components/AddNewToDo.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from './Modal';
 
 function AddNewToDo() {
-
+    const [showModal, setShowModal] = useState(false)
     return (
-        <div className='AddNewToDo'>
-            AddNewToDo
+        <div className='addNewToDo'>
+            <div className='btn'>
+                <button onClick={() => setShowModal(true)}>
+                    + New ToDo
+                </button>
+            </div>
+            
+            <Modal showModal={showModal} setShowModal={setShowModal}>
+                <div>
+                    Hello World
+                    <button
+                        onClick={() => setShowModal(false)}>
+                        hide
+                    </button>
+                </div>
+            </Modal>
         </div>
 
     )
