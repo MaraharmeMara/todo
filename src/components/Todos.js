59ddf6a238db9c140ef7f08d5d3dd5dc94e30dbd
MaraharmeMara <74717531+MaraharmeMara@users.noreply.github.com>
2024-05-28 11:07:25 +0300

    todos and todo changed

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,11 +3,46 @@ import Todo from './Todo';
 import Next7Days from './Next7Days';
 
 function Todos() {
+    const selectedProject = "today"
+    const todos = [
+        {
+            id : 'd54sd4',
+            text : "Go for a run",
+            time : "10:00 AM",
+            date : "06/03/2021",
+            day : "6",
+            checked : true,
+            color : '#adfa00',
+            project : 'personal'
+        },
+        {
+            id : 'd54fdf',
+            text : "Meeting",
+            time : "09:00 AM",
+            date : "08/03/2021",
+            day : "1",
+            checked : false,
+            color : '#00ff00',
+            project : 'work'
+        }
+    ]
+
 
     return (
         <div className='todos'>
-            <Todo />
-            <Next7Days />
+            <div className='selected-project'>
+                {selectedProject}
+            </div>
+            <div className='toDos'>
+                {
+                    selectedProject === "next 7 days" ?
+                    <Next7Days todos={todos} />
+                    :
+                    todos.map( todo => 
+                        <Todo todo={todo} key={todo.id} />
+                    )
+                }
+            </div>
         </div>
 
     )
