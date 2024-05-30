d505bee165a379417c620b7319465995c944745e
Ivan Cheremisin <fess932@gmail.com>
2024-05-30 23:27:36 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import { TodoContextProvider } from './context';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
     <TodoContextProvider>
       <App />
     </TodoContextProvider>
-  </StrictMode>
 );
