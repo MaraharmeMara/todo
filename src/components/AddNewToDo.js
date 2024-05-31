d7e09f3406c0ea7b7ce3b9873ee751023b01ebc3
Ivan Cheremisin <fess932@gmail.com>
2024-05-31 13:28:12 +0300

    fix

--- src/components/AddNewToDo.js
+++ src/components/AddNewToDo.js
@@ -1,79 +1,73 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Modal from './Modal';
-import TodoForm from './ToDoForm';
-import  { TodoContext } from '../context';
-import { calendarItems } from '../constants';
-import firebase from '../firebase';
-import moment from 'moment';
-import randomcolor from 'randomcolor';
-
+import React, { useContext, useEffect, useState } from "react";
+import Modal from "./Modal";
+import TodoForm from "./ToDoForm";
+import { TodoContext } from "../context";
+import { calendarItems } from "../constants";
+import firebase from "../firebase";
+import moment from "moment";
+import randomcolor from "randomcolor";
 
 function AddNewToDo() {
-    //CONTEXT
-    const { projects, selectedProject } = useContext(TodoContext)
-    //STATE
-    const [showModal, setShowModal] = useState(false)
-    const [text, setText] = useState('')
-    const [day, setDay] = useState(new Date())
-    const [time, setTime] = useState(new Date())
-    const [todoProject, setTodoProject] = useState(selectedProject)
+  //CONTEXT
+  const { projects, selectedProject } = useContext(TodoContext);
+  //STATE
+  const [showModal, setShowModal] = useState(false);
+  const [text, setText] = useState("");
+  const [day, setDay] = useState(new Date());
+  const [time, setTime] = useState(new Date());
+  const [todoProject, setTodoProject] = useState(selectedProject);
 
-function handleSubmit(e) {
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    if( text && !calendarItems.includes(todoProject)){
-        firebase
-            .firestore()
-            .collection('todos')
-            .add(
-                {
-                    text : text,
-                    date : moment(day).format('DD/MM/YYYY'),
-                    day : moment(day).format('d'),
-                    time : moment(time).format('hh:mm a'),
-                    checked : false,
-                    color : randomcolor(),
-                    projectName : todoProject,
-                }
-            )
+    if (text && !calendarItems.includes(todoProject)) {
+      firebase
+        .firestore()
+        .collection("todos")
+        .add({
+          text: text,
+          date: moment(day).format("DD/MM/YYYY"),
+          day: moment(day).format("d"),
+          time: moment(time).format("hh:mm a"),
+          checked: false,
+          color: randomcolor(),
+          projectName: todoProject,
+        });
 
-        setShowModal(false)
-        setText("")
-        setDay(new Date())
-        setTime(new Date())
+      setShowModal(false);
+      setText("");
+      setDay(new Date());
+      setTime(new Date());
     }
-}
+  }
 
-useEffect(() => {
-    setTodoProject(selectedProject)
-}, [selectedProject])
+  useEffect(() => {
+    setTodoProject(selectedProject);
+  }, [selectedProject]);
 
-    return (
-        <div className='addNewToDo'>
-            <div className='btn'>
-                <button onClick={() => setShowModal(true)}>
-                    + New ToDo
-                </button>
-            </div>
-            <Modal showModal={showModal} setShowModal={setShowModal}>
-                <TodoForm
-                    handleSubmit={handleSubmit}
-                    heading='Add New To Do'
-                    text={text}
-                    setText={setText}
-                    day={day}
-                    setDay={setDay}
-                    time={time}
-                    setTime={setTime}
-                    todoProject={todoProject}
-                    setTodoProject={setTodoProject}
-                    projects={projects}
-                    showButtons={true}
-                    setShowModal={setShowModal}
-                />
-            </Modal>
-        </div>
-
-    )
+  return (
+    <div className="addNewToDo">
+      <div className="btn">
+        <button onClick={() => setShowModal(true)}>+ New ToDo</button>
+      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <TodoForm
+          handleSubmit={handleSubmit}
+          heading="Add New To Do"
+          text={text}
+          setText={setText}
+          day={day}
+          setDay={setDay}
+          time={time}
+          setTime={setTime}
+          todoProject={todoProject}
+          setTodoProject={setTodoProject}
+          projects={projects}
+          showButtons={true}
+          setShowModal={setShowModal}
+        />
+      </Modal>
+    </div>
+  );
 }
 export default AddNewToDo;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import './index.css';
-import App from './App';
-import { TodoContextProvider } from './context';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import { TodoContextProvider } from "./context";
+import { createRoot } from "react-dom/client";
 
-
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
+  <React.StrictMode>
     <TodoContextProvider>
       <App />
     </TodoContextProvider>
+  </React.StrictMode>
 );
