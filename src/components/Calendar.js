95bfa1ad0f481708a4fe5f508820d3924f5ed160
Ivan Cheremisin <fess932@gmail.com>
2024-05-31 13:15:26 +0300

    commit

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,38 +1,35 @@
-import React, { useContext } from 'react';
-import { CalendarDate, CaretUp } from 'react-bootstrap-icons';
-import { calendarItems } from '../constants';
-import { TodoContext } from '../context';
+import React, { useContext } from "react";
+import { CalendarDate, CaretUp } from "react-bootstrap-icons";
+import { calendarItems } from "../constants";
+import { TodoContext } from "../context";
 
 function Calendar() {
-    const { setSelectedProject } = useContext(TodoContext)
-    return (
-        <div className='calendar'>
-            <div className='header'>
-                <div className='title'>
-                    <CalendarDate size="18" />
-                    <p>Calendar</p>
-                </div>
-                <div className='btns'>
-                    <span>
-                        <CaretUp size="20" />
-                    </span>
-                </div>
-                <div className='items'>
-                    {
-                        calendarItems.map( item => 
-                            <div 
-                                className='item' 
-                                key={item}
-                                onClick={ () => setSelectedProject(item)}
-                            >
-                                {item}
-                            </div>
-                        )
-                    }
-                </div>
-            </div>
+  const { setSelectedProject } = useContext(TodoContext);
+  return (
+    <div className="calendar">
+      <div className="header">
+        <div className="title">
+          <CalendarDate size="18" />
+          <p>Calendar</p>
         </div>
-
-    )
+        <div className="btns">
+          <span>
+            <CaretUp size="20" />
+          </span>
+        </div>
+      </div>
+      <div className="items">
+        {calendarItems.map((item) => (
+          <div
+            className="item"
+            key={item}
+            onClick={() => setSelectedProject(item)}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Calendar;
