0af2fc0497631dd03d693b634eb3aad1b8f0706d
MaraharmeMara <kotova.mariia94@gmail.com>
2024-06-05 12:11:32 +0300

    trying to create a context.provider

---
+++ src/context/LoginContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+
+const EmailContext = createContext();
+
+function EmailContextProvider({ children }) {
+  const defaultEmail = "";
+  const [changeUser, setChangeUser] = useState(email);
+  return (
+    <EmailContext.Provider
+      value={(defaultEmail, changeUser, setChangeUser)}
+    ></EmailContext.Provider>
+  );
+}
+
+export { EmailContextProvider, EmailContext };
