acd1c2006c6d12cfcb5d67da3aff63cd1920af5f
Ivan Cheremisin <fess932@gmail.com>
2024-06-06 10:23:31 +0300

    fix

--- src/components/User.js
+++ src/components/User.js
@@ -8,18 +8,12 @@ import { EmailContext } from "../context/LoginContext";
 
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
-import firebase from "../firebase";
 
 function User() {
   const [showModal, setShowModal] = useState(false);
-  const [signIn, setSignIn] = useState(false);
   const [current, setCurrent] = useState(0);
   const { changeUser } = useContext(EmailContext);
   const { setChangeUser } = useContext(EmailContext);
-
-  const handleLogout = () => {
-    return firebase.auth().signOut;
-  };
 
   function changeModal() {
     if (current === 0) {
@@ -29,8 +23,14 @@ function User() {
     }
   }
 
-  function logOut() {
-    return { handleLogout }, setChangeUser("");
+  async function handleLogout() {
+    try {
+      const a = await signOut(auth);
+      console.log("logout", a);
+      setChangeUser("");
+    } catch (e) {
+      console.log("error", e);
+    }
   }
 
   return (
@@ -45,10 +45,15 @@ function User() {
             Log In
           </button>
         )}
-        {changeUser !== "" && <span>Email: {changeUser}</span>}
-        <button className="log" onClick={logOut}>
-          Log Out
-        </button>
+        {changeUser !== "" && (
+          <>
+            {" "}
+            <span>Email: {changeUser}</span>
+            <button className="log" onClick={handleLogout}>
+              Log Out
+            </button>
+          </>
+        )}
 
         <Modal showModal={showModal} setShowModal={setShowModal}>
           {current === 0 && (
